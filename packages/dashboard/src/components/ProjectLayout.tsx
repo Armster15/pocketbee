160710f61c690ab005c20ed20b404572cad69b6e
Armaan A <armaanaggarwal@gmail.com>
2023-10-26 00:04:05 -0700

    make dashboard buttons look nicer

--- packages/dashboard/src/components/ProjectLayout.tsx
+++ packages/dashboard/src/components/ProjectLayout.tsx
@@ -2,6 +2,8 @@ import { type PropsWithChildren, type ComponentProps } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { api } from "$/utils/api";
+import clsx from "clsx";
+import { IoStatsChart, IoSettings } from "react-icons/io5";
 
 export const ProjectLayout = ({ children }: PropsWithChildren) => {
   const router = useRouter();
@@ -17,9 +19,13 @@ export const ProjectLayout = ({ children }: PropsWithChildren) => {
       <aside className="mr-12 w-56">
         <h2 className="truncate text-2xl font-semibold">{project?.name}</h2>
 
-        <div className="grid">
-          <ActiveLink href={`/project/${projectId}`}>Analytics</ActiveLink>
+        <div className="mt-5 grid gap-1">
+          <ActiveLink href={`/project/${projectId}`}>
+            <IoStatsChart aria-hidden />
+            Analytics
+          </ActiveLink>
           <ActiveLink href={`/project/${projectId}/settings`}>
+            <IoSettings aria-hidden />
             Settings
           </ActiveLink>
         </div>
@@ -37,6 +43,13 @@ const ActiveLink = ({ href, ...props }: ComponentProps<typeof Link>) => {
     new URL(router.asPath, "https://example.com").pathname;
 
   return (
-    <Link className={active ? "bg-yellow-300" : ""} href={href} {...props} />
+    <Link
+      className={clsx(
+        "flex items-center gap-2 rounded-lg px-3 py-2 text-gray-700 duration-100",
+        active ? "bg-gray-200/80" : "hover:bg-gray-100",
+      )}
+      href={href}
+      {...props}
+    />
   );
 };
