1389e724b1a17d255fa40377149abefb2edb86d8
Armaan A <armaanaggarwal@gmail.com>
2023-10-30 00:18:05 -0700

    forwardref in button

--- packages/dashboard/src/components/Button.tsx
+++ packages/dashboard/src/components/Button.tsx
@@ -1,14 +1,18 @@
-import type { ButtonProps } from "react-html-props";
+import { forwardRef } from "react";
 import clsx from "clsx";
+import type { ButtonProps } from "react-html-props";
 
-export const Button = ({ className, ...props }: ButtonProps) => {
-  return (
-    <button
-      className={clsx(
-        "rounded-xl border-2 p-3 py-2 duration-100 hover:bg-gray-50/95 active:bg-gray-100",
-        className,
-      )}
-      {...props}
-    />
-  );
-};
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  function Button({ className, ...props }, ref) {
+    return (
+      <button
+        className={clsx(
+          "rounded-xl border-2 p-3 py-2 duration-100 hover:bg-gray-50/95 active:bg-gray-100",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
