c0af4d4e2e9338e1b235a5bd9042b6676b041a68
Armaan A <armaanaggarwal@gmail.com>
2023-11-01 16:49:16 -0700

    make input rounded-xl not 2xl

--- packages/dashboard/src/components/Input.tsx
+++ packages/dashboard/src/components/Input.tsx
@@ -8,7 +8,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
 ) {
   return (
     <input
-      className={clsx("rounded-2xl border-2 p-2", className)}
+      className={clsx("rounded-xl border-2 p-2", className)}
       ref={ref}
       {...props}
     />
